bcf31bb127c8c7b50ac49e3d92b527314e8b68cd
Patrik Fors <patrik.fors@gmail.com>
2020-12-03 10:49:40 +0000

    part1 complete

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,18 +1,35 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 #include "input.h"
 
+using int_t = uint32_t;
+
 int main(int argc, char *argv[])
 {
-   std::vector<std::string> args(argv, argv + argc);
-   std::vector<std::string> input;
-   input::get_input(args, input);
+    std::vector<std::string> args(argv, argv + argc);
+    std::vector<std::string> input_strings;
+    input::get_input(args, input_strings);
 
-   for(auto x : input) {
-       std::cout << x << std::endl;
-   }
+    std::vector<int_t> input;
 
-   return 0;
+    std::transform(input_strings.begin(), input_strings.end(), std::back_inserter(input),
+                   [](std::string s) -> int_t { 
+                       return std::stol(s); });
+
+    std::unordered_set<int_t> data(std::begin(input), std::end(input));
+
+    for (auto d : data)
+    {
+        if (data.count(2020-d)) {
+            std::cout << "Found matching set: " << d << ", " << (2020-d) << std::endl;
+            std::cout << "Product is: " << d*(2020-d) << std::endl;
+        }
+        
+    }
+
+    return 0;
 }
